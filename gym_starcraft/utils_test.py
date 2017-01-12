71e366672c09e28c8306ea431f62d143064108b7
haitao.lht <haitao.lht@taobao.com>
2017-01-12 09:17:08 +0800

    Refactor utils test

--- gym_starcraft/utils_test.py
+++ gym_starcraft/utils_test.py
@@ -1,20 +1,30 @@
-import utils
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import unittest
+import gym_starcraft.utils as utils
+
+
+class UtilsTest(unittest.TestCase):
+    def test_get_degree(self):
+        self.assertEquals(utils.get_degree(10, -10, 10, -20), -90)
+        self.assertEquals(utils.get_degree(10, -10, 10, -5), 90)
+        self.assertEquals(utils.get_degree(10, -10, 5, -10), 180)
+        self.assertEquals(utils.get_degree(10, -10, 15, -10), 0)
+        self.assertEquals(utils.get_degree(10, -10, 20, -20), -45)
+
+    def test_get_distance(self):
+        self.assertEquals(utils.get_distance(10, -10, 10, -20), 10)
+        self.assertEquals(utils.get_distance(10, -10, 10, -5), 5)
+        self.assertEquals(utils.get_distance(10, -10, 5, -10), 5)
+        self.assertEquals(utils.get_distance(10, -10, 15, -10), 5)
+
+    def test_get_position(self):
+        self.assertEquals(utils.get_position(90, 10, 10, -10), (10, 0))
+        self.assertEquals(utils.get_position(0, 10, 10, -10), (20, -10))
+        self.assertEquals(utils.get_position(180, 10, 10, -10), (0, -10))
+
 
 if __name__ == '__main__':
-    assert utils.get_degree(10, -10, 10, -20) == -90
-    assert utils.get_degree(10, -10, 10, -5) == 90
-    assert utils.get_degree(10, -10, 5, -10) == 180
-    assert utils.get_degree(10, -10, 15, -10) == 0
-    assert utils.get_degree(10, -10, 20, -20) == -45
-
-    assert utils.get_distance(10, -10, 10, -20) == 10
-    assert utils.get_distance(10, -10, 10, -5) == 5
-    assert utils.get_distance(10, -10, 5, -10) == 5
-    assert utils.get_distance(10, -10, 15, -10) == 5
-
-    assert utils.get_position(90, 10, 10, -10) == (10, 0)
-    assert utils.get_position(0, 10, 10, -10) == (20, -10)
-    assert utils.get_position(180, 10, 10, -10) == (0, -10)
-    x1, y1 = utils.get_position(-90, 10, 10, -10)
-    assert int(x1) == 10
-    assert int(y1) == -20
+    unittest.main()
